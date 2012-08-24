cee3bab27125c8be810f55198274314aa2f144ed
kienjakenobi <utterrot@gmail.com>
2012-08-24 14:46:34 -0500

    updated psd script, removed rescale.py reference, minor PV changes

--- atlas_opi/psd.sh
+++ atlas_opi/psd.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+touch HERE
 caput LLRF4:FILE0:Capture 1
 WAIT=`caget -t LLRF4:FILE0:FileDurTime`
 TOTALWAIT=`echo "$WAIT+2" | bc`
 sleep $TOTALWAIT
-FILEPATH=`caget -t LLRF4:FILE0:FullFileName_RBV`
+FILEPATH=`caget -St LLRF4:FILE0:FullFileName_RBV`
+../sdds/plotPSD $FILEPATH
