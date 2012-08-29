45ec989856bfa637050afa56c911caefee8d5708
b225404 <ajd@anl.gov>
2012-08-29 17:04:40 -0500

    updated scripts.opi and psd.sh but it still doesn't work due to working path problem

--- atlas_opi/psd.sh
+++ atlas_opi/psd.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
-touch HERE
+
+# The EPICS extensions version of caget does not support -S string
+#	conversion.  Hence, we use the EPICS base version.  A linux-x86_64
+#	binary has been included in the sdds directory
+
+# Hit save button by setting save PV
 caput LLRF4:FILE0:Capture 1
-WAIT=`caget -t LLRF4:FILE0:FileDurTime`
+
+# Get the file save duration time as a variable
+WAIT=`../../sdds/caget_v2 -t LLRF4:FILE0:FileDurTime`
+
+# Add 2 seconds to the total wait time
 TOTALWAIT=`echo "$WAIT+2" | bc`
+
+# Wait for the necessary amount of time
 sleep $TOTALWAIT
-FILEPATH=`caget -St LLRF4:FILE0:FullFileName_RBV`
-../sdds/plotPSD $FILEPATH
+
+# Get the save path as a string and execute sdds using it
+FILEPATH=`../../sdds/caget_v2 -St LLRF4:FILE0:FullFileName_RBV`
+../../sdds/plotPSD $FILEPATH
