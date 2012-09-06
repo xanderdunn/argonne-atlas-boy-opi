53f9de5fb0b5eb8af418e367fb3cb37283be0216
alex_dunn <ajd@anl.gov>
2012-09-06 09:59:19 -0500

    made the save toggle script a file, added it to the Data Buffer section NOW button

---
+++ atlas_opi/scripts/save_toggle.py
@@ -0,0 +1,30 @@
+from org.csstudio.opibuilder.scriptUtil import PVUtil
+
+# If it is taking data, then stop by writing 0
+# It it is not taking data, then start by writing 1
+
+#PVs
+# pvs[0] = $(TS)$(LLRF):FILE0:Capture
+
+# Get macros
+ts = display.getMacroValue("TS")
+llrf = display.getMacroValue("LLRF")
+pv = ts + llrf + ":FILE0:Capture"
+
+get_pv = display.getWidget("Capture_Indicator").getPV()
+set_pv = display.getWidget("Save_Button").getPV()
+
+def main():
+	# Get current PV value
+	curr = PVUtil.getLong(get_pv)
+	# If currently 0, then set to 1
+	if curr == 0:
+		print "Setting to 1"
+		set_pv.setValue(1)
+	# If currently 1, then set to 0
+	if curr == 1:
+		print "Setting to 0"
+		set_pv.setValue(0)
+
+if __name__ == '__main__':
+    main()
