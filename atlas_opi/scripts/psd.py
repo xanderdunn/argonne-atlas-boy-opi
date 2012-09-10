bc24d7b463b04b708997e91debb7953a2a2c438c
alex_dunn <ajd@anl.gov>
2012-09-10 13:46:53 -0500

    fixed improper use of cwd argument on os.path.abspath

--- atlas_opi/scripts/psd.py
+++ atlas_opi/scripts/psd.py
@@ -29,14 +29,22 @@ wait = p.communicate()[0]
 # Add 2 seconds to the total wait time and sleep for that time
 time.sleep(float(wait) + 2)
 
-# Get the file path as a string
-# This is run from ops/cavCtl/css/CSS_EPICS/
+# Get the absolute path of the plotPSD script
+# This is run from the css/CSS_EPICS directory
+plotpath = os.path.abspath("../../sdds/plotPSD")
+
+# Move into the spxrfshare directory, which is what the data file paths are
+#    relative to
+os.chdir("../../../../")
+
+# Get the data file path as a string
 p = subprocess.Popen(["../../sdds/caget_v2", "-St", "LLRF4:FILE0:FullFileName_RBV"], stdout=subprocess.PIPE)
 filepath = p.communicate()[0] # Get the output of the above command
-filepath = os.path.abspath(filepath) # Make it an absolute path
-# If the given path is relative, make it absolute relative to the spxrfshare directory
-plotpath = os.path.abspath(["../../sdds/plotPSD"], cwd="../../../../") # Get absolute path of plotPSD
-# The following is run from the directory of the user's save file
-# runpath = os.path.split(filepath)[0] # Get just the directory of the save file
-runpath = os.path.split(plotpath)[0] # Get just the directory of the plotter location
-subprocess.Popen([plotpath, filepath], cwd=runpath) # Run plotPSD on the saved file
+# If the user defined an absolute path, then it will be unchanged.
+# If the user defined a relative path, it will be made absolute relative to
+#    spxrfshare
+filepath = os.path.abspath(filepath)
+
+# run plotPSD from the directory of the user's data file
+runpath = os.path.split(filepath)[0] # Get just the directory of the data file
+subprocess.Popen([plotpath, filepath], cwd=runpath) # Run plotPSD on the data file
