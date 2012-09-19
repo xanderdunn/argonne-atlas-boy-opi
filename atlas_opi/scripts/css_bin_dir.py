67f64b5feaf52c7407d088d0d33d14f9ff9023d7
alex_dunn <ajd@anl.gov>
2012-09-19 10:23:50 -0500

    improved print statements and bug fixed one of the system variable names

--- atlas_opi/scripts/css_bin_dir.py
+++ atlas_opi/scripts/css_bin_dir.py
@@ -9,6 +9,7 @@ import os # for getting os environment variables
 css_dir_var = System.getProperty("osgi.install.area")
 css_dir = css_dir_var.split(":")[1]
 System.setProperty("css.dir", css_dir)
+print "CSS binary dir:", css_dir
 
 # Define the medm directory
 medm_dir= os.path.normpath(os.path.join(css_dir, "../../medm/"))
@@ -26,10 +27,10 @@ print "Host arch:", host_arch
 
 # Define the full caxgui directory
 System.setProperty("caxgui.full.dir", caxgui_dir + "/bin/" + host_arch + "/")
-print "Full caxgui dir:", "caxgui.full.dir", caxgui_dir + "/bin/" + host_arch + "/"
+print "Full caxgui dir:", caxgui_dir + "/bin/" + host_arch + "/"
 
 # Define the location of the workspace
-workspace_dir_var = System.getProperty("osgi.install.area")
+workspace_dir_var = System.getProperty("osgi.instance.area")
 workspace_dir = workspace_dir_var.split(":")[1]
 System.setProperty("user.workspace", workspace_dir)
 print "User workspace:", workspace_dir
