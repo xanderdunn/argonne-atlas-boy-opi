fc8d62f84cda2151647be924a46963a74606c091
alex_dunn <ajd@anl.gov>
2012-09-19 11:15:43 -0500

    bug fix: made the last Popen call a single string command

--- atlas_opi/scripts/psd.py
+++ atlas_opi/scripts/psd.py
@@ -58,5 +58,5 @@ script_path = workspace + "atlas_opi/scripts/psd.sh"
 
 # run plotPSD from the directory of the user's data file
 runpath = os.path.split(filepath)[0] # Get just the directory of the data file
-subprocess.Popen([script_path, plotpath, filepath, str(welch1), str(welch2)], cwd=runpath, shell=True)
+subprocess.Popen([script_path + " " + plotpath + " " + filepath + " " + str(welch1) + " " + str(welch2)], cwd=runpath, shell=True)
 # subprocess.Popen([plotpath, filepath, str(welch1), str(welch2)], cwd=runpath) # Run plotPSD on the data file
